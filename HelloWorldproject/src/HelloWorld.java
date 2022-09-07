7bc9a909559747bc7bc4399f9f9e961284fc0cea
hp <hp@DESKTOP-CHPLIC2>
2022-09-06 23:16:53 -0700

    first commit

---
+++ HelloWorldproject/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2082;
+		if(a%4==0)
+		{
+		System.out.println("leap error");
+		}
+		else
+		{
+			System.out.println("not a leap year");
+		}
+   }
+
+}
---
+++ HelloWorldproject/src/forloop.java
@@ -0,0 +1,13 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=2;
+		for(int i=1; i <= 10; i++)  
+			
+			System.out.println(num+" * "+i+" = "+num*i); 
+
+	}
+
+}

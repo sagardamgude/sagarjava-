ebd89202912f69098ec752c299e29a1e1fbdd6a7
sagardamgude <113004878+sagardamgude@users.noreply.github.com>
2022-09-07 23:08:44 -0700

    Add files via upload

---
+++ Grettestnum.java
@@ -0,0 +1,21 @@
+
+public class Grettestnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l=29;
+		int m=23;
+		int n=12;
+		 if( l >= m && l >= n)
+	            System.out.println(l + " is the largest number.");
+
+	        else if (m >= l && m >= n)
+	            System.out.println(m + " is the largest number.");
+
+	        else
+	            System.out.println(n + " is the largest number.");
+		
+
+	}
+
+}
